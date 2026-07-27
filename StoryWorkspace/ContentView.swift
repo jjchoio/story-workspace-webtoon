93c04f88743da5d59e9b280a981251f4f4f4c818
Joshua Choi <im.joshuac@gmail.com>
2026-07-27 13:30:00 -0700

    Initial Commit

---
+++ StoryWorkspace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoryWorkspace
+//
+//  Created by Joshua Choi on 7/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoryWorkspace/StoryWorkspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryWorkspaceApp.swift
+//  StoryWorkspace
+//
+//  Created by Joshua Choi on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct StoryWorkspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
