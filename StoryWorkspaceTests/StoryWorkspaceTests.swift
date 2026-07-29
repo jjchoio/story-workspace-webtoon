8d193f98aede75131d9229226b51231a067fb855
Joshua Choi <im.joshuac@gmail.com>
2026-07-28 21:03:33 -0700

    Add StoryWorkspaceTests target using Swift Testing

---
+++ StoryWorkspaceTests/StoryWorkspaceTests.swift
@@ -0,0 +1,16 @@
+//
+//  StoryWorkspaceTests.swift
+//  StoryWorkspaceTests
+//
+//  Created by Joshua Choi on 7/28/26.
+//
+
+import Testing
+
+struct StoryWorkspaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
